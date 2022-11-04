1f229e707d8b9952e34c8bd340e2af202b0cc7c8
Vasyl <vasyl.ivasyk@gmail.com>
2022-11-04 22:05:35 +0200

    Add Header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartialsComponent } from './components/partials/partials.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartialsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
