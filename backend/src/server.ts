6ad54d8cce5f9b55e0a810295b9c25602b94c81f
Vasyl <vasyl.ivasyk@gmail.com>
2022-11-09 22:35:45 +0200

    Part  12.1 Login page

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
-import { sample_foods, sample_tags } from "./data";
+import { sample_foods, sample_tags, sample_users } from "./data";
+import  jwt from "jsonwebtoken"
 
 const app = express();
+app.use(express.json())
 app.use(cors({
    credentials:true,
    origin : ["http://localhost:4200"]
-})
-)
+}))
+
 
 app.get("/api/foods", (req,res) => {
    res.send(sample_foods)
@@ -34,8 +36,28 @@ app.get("/api/foods/:foodId", (req,res) => {
    const foodId = req.params.foodId
    const food = sample_foods.find(food => food.id == foodId)
    res.send(food)
-
 })
+
+app.post("/api/users/login", (req,res) => {
+      const {email, password} = req.body
+      const user =  sample_users.find(user => user.email === email && user.password === password)
+      if (user) {
+         res.send(generateTokenResponce(user))
+      } else {
+         res.status(400).send("User name or password is not valid")
+      }
+})
+
+const generateTokenResponce = (user: any) => {
+    const  token = jwt.sign({
+      email: user.emails,
+      isAdmin : user.isAdmin
+   }, "SomeRandomRoken",{expiresIn:"30d"}
+   )
+   user.token = token
+   return user
+}
+
 
 
 const port = 5000
